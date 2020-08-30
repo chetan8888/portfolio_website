6c1b8fcc29d0c84bf0543c462d9a8772a313b434
chetan <chetangour88@gmail.com>
2020-08-30 18:21:35 +0530

    skills updated

--- src/constants/Skills.js
+++ src/constants/Skills.js
@@ -1,37 +1,38 @@
 export const skills = {
-    heading: "Skills",
-    quote: "Build your skills, not your resume",
-    techSkills: [
-        "JavaScript",
-        "NodeJS",
-        "ReactJS",
-        "C++",
-        "C",
-        "Python",
-        "HTML",
-        "CSS",
-        "jQuery",
-        "Bootstrap",
-        "Git",
-        "GitHub",
-        "MongoDB",
-    ],
-    industryKnowledge: [
-        "Software",
-        "Web Development",
-        "Frontend",
-        "Backend",
-        "Design",
-        "Database Management"
-    ],
-    interpersonalSkills: [
-        "Communication",
-        "Teaching",
-        "Time Management",
-        "Leadership",
-        "Management",
-        "Presentations"
-    ]
-}
+  heading: 'Skills',
+  quote: 'Build your skills, not your resume',
+  techSkills: [
+    'JavaScript',
+    'NodeJS',
+    'ReactJS',
+    'C++',
+    'C',
+    'Python',
+    'HTML',
+    'CSS',
+    'jQuery',
+    'Bootstrap',
+    'Git',
+    'GitHub',
+    'Simulink',
+    'MongoDB',
+  ],
+  industryKnowledge: [
+    'Software',
+    'Web Development',
+    'Frontend',
+    'Backend',
+    'Design',
+    'Database Management',
+  ],
+  interpersonalSkills: [
+    'Communication',
+    'Teaching',
+    'Time Management',
+    'Leadership',
+    'Management',
+    'Presentations',
+  ],
+};
 
-export default skills
+export default skills;
